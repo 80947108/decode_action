23c1071d92dd58c1d7926a78afe1a77a6a031896
action <action@github.com>
2025-12-15 06:31:52 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,119 +1,26 @@
-//Mon Dec 15 2025 06:10:58 GMT+0000 (Coordinated Universal Time)
+//Mon Dec 15 2025 06:31:52 GMT+0000 (Coordinated Universal Time)
 //Base:<url id="cv1cref6o68qmpt26ol0" type="url" status="parsed" title="GitHub - echo094/decode-js: JS混淆代码的AST分析工具 AST analysis tool for obfuscated JS code" wc="2165">https://github.com/echo094/decode-js</url>
 //Modify:<url id="cv1cref6o68qmpt26olg" type="url" status="parsed" title="GitHub - smallfawn/decode_action: 世界上本来不存在加密，加密的人多了，也便成就了解密" wc="741">https://github.com/smallfawn/decode_action</url>
-function main(_0x1d6f2e) {
-  const _0x28b7dd = _0x1d6f2e.url,
-    _0x29beb7 = ku9.getQuery(_0x28b7dd, "id") || "szws",
-    _0x4c05a2 = {
-      "dnzh": ["7gf6a72", "http://vod.center.hljtv.com/lsdream/"],
-      "mlzh": ["wy7HG9x", "http://vod.center.hljtv.com/lsdream/"],
-      "sfhzh": ["gS7S5pS", "http://vod.center.hljtv.com/lsdream/"],
-      "jyzh": ["1lqKC4B", "http://vod.center.hljtv.com/lsdream/"],
-      "tlzh": ["QgOu88F", "http://vod.center.hljtv.com/lsdream/"],
-      "nhzh": ["Ig4YRW0", "http://vod.center.hljtv.com/lsdream/"],
-      "yazh": ["1szOvMr", "http://vod.center.hljtv.com/lsdream/"],
-      "yyzh": ["d037rmI", "http://vod.center.hljtv.com/lsdream/"],
-      "sxiws": ["q8RVWgs", "https://livehhhttps.sxrtv.com/lsdream/"],
-      "sxjj": ["4j01KWX", "https://livehhhttps.sxrtv.com/lsdream/"],
-      "sxys": ["Md571Kv", "https://livehhhttps.sxrtv.com/lsdream/"],
-      "sxshfz": ["p4y5do9", "https://livehhhttps.sxrtv.com/lsdream/"],
-      "sxwtsh": ["Y00Xezi", "https://livehhhttps.sxrtv.com/lsdream/"],
-      "sxhh": ["lce1mC4", "https://livehhhttps.sxrtv.com/lsdream/"],
-      "dlzh": ["tcb3IB5", "http://live.dltv.cn/"],
-      "dlsh": ["JzcFkF4", "http://live.dltv.cn/"],
-      "dlwt": ["hxT7Fc3", "http://live.dltv.cn/"],
-      "dlzhys": ["8cuL6wa", "http://live.dltv.cn/"],
-      "dlse": ["q6tZ6Ba", "http://live.dltv.cn/"],
-      "dlgw": ["N4S4uAj", "http://live.dltv.cn/"],
-      "sxws": ["X6feiLQ", "http://live.sxtvs.com.cn/lsdream/"],
-      "sxxwzx": ["SeT8Mem", "http://live.sxtvs.com.cn/lsdream/"],
-      "sxdsqc": ["zAJprk2", "http://live.sxtvs.com.cn/lsdream/"],
-      "sxtyxx": ["LLhVrGZ", "http://live.sxtvs.com.cn/lsdream/"],
-      "sxsh": ["a73O8Sd", "http://live.sxtvs.com.cn/lsdream/"],
-      "sxgg": ["8aCj3gK", "http://live.sxtvs.com.cn/lsdream/"],
-      "sxyd": ["Au7pR2j", "http://live.sxtvs.com.cn/lsdream/"],
-      "xaxwzh": ["BmS21Wa", "http://live.sxtvs.com.cn/lsdream/"],
-      "ylxwzh": ["EQiV8hZ", "http://live.sxtvs.com.cn/lsdream/"],
-      "xyxwzh": ["9xmmLy4", "http://live.sxtvs.com.cn/lsdream/"],
-      "yaxwzh": ["14c199y", "http://live.sxtvs.com.cn/lsdream/"],
-      "tcxwzh": ["78d3NMg", "http://live.sxtvs.com.cn/lsdream/"],
-      "akxwzh": ["ebUpMpG", "http://live.sxtvs.com.cn/lsdream/"],
-      "wnxwzh": ["BmSLiAx", "http://live.sxtvs.com.cn/lsdream/"],
-      "slxwzh": ["21HiFQt", "http://live.sxtvs.com.cn/lsdream/"],
-      "ylinxwzh": ["Uys5UFs", "http://live.sxtvs.com.cn/lsdream/"],
-      "hzxwzh": ["mul2l4m", "http://live.sxtvs.com.cn/lsdream/"],
-      "tyxwzh": ["49VAfrw", "https://tytv-hls.sxtygdy.com/"],
-      "tyjjsh": ["u8BmT6h", "https://tytv-hls.sxtygdy.com/"],
-      "tysjfz": ["phsry3e", "https://tytv-hls.sxtygdy.com/"],
-      "tyys": ["J4EX72D", "https://tytv-hls.sxtygdy.com/"],
-      "tywt": ["rk8Z088", "https://tytv-hls.sxtygdy.com/"],
-      "tyblg": ["iancgyD", "https://tytv-hls.sxtygdy.com/"],
-      "tycssh": ["i88rmGU", "https://tytv-hls.sxtygdy.com/"],
-      "tyjy": ["g4XtSCF", "https://tytv-hls.sxtygdy.com/"],
-      "stxwzh": ["lKGXIQa", "http://sttv-hls.strtv.cn", 1],
-      "sttvjj": ["7xjJK9d", "http://sttv-hls.strtv.cn", 1],
-      "sttvys": ["G7Kql7a", "http://sttv-hls.strtv.cn", 1],
-      "stfmjj": ["L3y6rt8", "http://sttv-hls.strtv.cn"],
-      "stfmyy": ["s7k681h", "http://sttv-hls.strtv.cn"],
-      "stfmzh": ["Li7mg21", "http://sttv-hls.strtv.cn"],
-      "jmzh": ["4x3T0dO", "http://tidelive.jmtv.cn/lsdream/"],
-      "jmqxsh": ["qzohscw", "http://tidelive.jmtv.cn/lsdream/"],
-      "zttv1": ["Ii3aRRS", "https://live.zttv.cn/lsdream/"],
-      "zttv3": ["EiCgjnA", "https://live.zttv.cn/lsdream/"],
-      "szws": ["AxeFRth", "https://sztv-live.sztv.com.cn", 1],
-      "szyl": ["1q4iPng", "https://sztv-live.sztv.com.cn", 1],
-      "szse": ["1SIQj6s", "https://sztv-live.sztv.com.cn", 1],
-      "szgg": ["2q76Sw2", "https://sztv-live.sztv.com.cn", 1],
-      "szcjsh": ["3vlcoxP", "https://sztv-live.sztv.com.cn", 1],
-      "szdsj": ["4azbkoY", "https://sztv-live.sztv.com.cn", 1],
-      "szds": ["ZwxzUXr", "https://sztv-live.sztv.com.cn", 1],
-      "szgj": ["sztvgjpd", "https://sztv-live.sztv.com.cn"],
-      "szyd": ["wDF6KJ3", "https://sztv-live.sztv.com.cn", 1],
-      "yhgw": ["BJ5u5k2", "https://sztv-live.sztv.com.cn", 1],
-      "sztyjk": ["sztvtyjk", "https://sztv-live.sztv.com.cn", 1],
-      "ntxwzh": ["4f1", "https://nt-hls.ntjoy.com/live/", 2]
-    },
-    _0x5a6d86 = Math.floor(Date.now() / 1000),
-    _0x1cd908 = ku9.md5(_0x5a6d86 + _0x4c05a2[_0x29beb7][0] + "cutvLiveStream|Dream2017"),
-    _0x581607 = "http://hls-api.sztv.com.cn/getCutvHlsLiveKey?t=" + _0x5a6d86 + "&token=" + _0x1cd908 + "&id=" + _0x4c05a2[_0x29beb7][0],
-    _0x18941d = {
-      "User-Agent": "Mozilla/5.0"
+function main(_0xe087bf) {
+  let _0x4413ea = _0xe087bf.url,
+    _0x19b0ef = ku9.getQuery(_0x4413ea, "id");
+  const _0x2c7974 = "https://api.live.bilibili.com/xlive/web-room/v2/index/getRoomPlayInfo?device=phone&platform=html5&scale=3&build=10000&protocol=0,1&format=0,1,2&codec=0,1&room_id=" + _0x19b0ef,
+    _0x642e36 = {
+      "User-Agent": "Mozilla/5.0",
+      "Referer": "https://live.bilibili.com/h5/" + _0x19b0ef
     };
-  let _0x32e396;
-  try {
-    _0x32e396 = ku9.get(_0x581607, JSON.stringify(_0x18941d));
-  } catch (_0x5aeeb4) {
-    return JSON.stringify({
-      "error": "Failed to fetch data: " + _0x5aeeb4.message,
-      "url": _0x581607
-    });
-  }
-  if (!_0x32e396) return JSON.stringify({
-    "error": "Empty response from server",
-    "url": _0x581607
+  let _0xe2cd7c = ku9.get(_0x2c7974, JSON.stringify(_0x642e36));
+  _0xe2cd7c = _0xe2cd7c.replace(/\\u0026/g, "&");
+  var _0x18982b = /"fmp4","([^"]+)".*?"codec_name":"([^"]+)".*?"base_url":"([^"]+)".*?"host":"([^"]+)".*?"extra":"([^"]+)"/s,
+    _0x3a340d = _0xe2cd7c.match(_0x18982b),
+    _0x19b507 = _0x3a340d[0],
+    _0x45ebf9 = _0x3a340d[1],
+    _0x1a2bdf = _0x3a340d[2],
+    _0x42ff55 = _0x3a340d[3],
+    _0x34db93 = _0x3a340d[4],
+    _0x3e5751 = _0x34db93 + _0x42ff55;
+  return JSON.stringify({
+    "url": _0x3e5751,
+    "headers": _0x642e36
   });
-  const _0x2ac0ce = _0x32e396 || "defaultPname";
-  if (!_0x4c05a2[_0x29beb7][2]) {
-    const _0x51daf4 = _0x4c05a2[_0x29beb7][1] + _0x4c05a2[_0x29beb7][0] + "/" + _0x2ac0ce + ".m3u8";
-    return JSON.stringify({
-      "url": _0x51daf4
-    });
-  }
-  if (_0x4c05a2[_0x29beb7][2] === 1) {
-    const _0xfc5e34 = _0x29beb7.toLowerCase();
-    let _0xbabeac = {
-      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36",
-      "Referer": "https://www.sztv.com.cn/pindao/index.html?id=7868",
-      "Origin": "https://www.sztv.com.cn"
-    };
-    const _0x1aa141 = (Math.floor(Date.now() / 1000) + 7200).toString(16),
-      _0x300dce = _0xfc5e34.startsWith("st") ? "bf9b2cab35a9c38857b82aabf99874aa96b9ffbb" : "ejow6p6p6hmrm9g96beh2knecdq5kyv9bp0zxyg7",
-      _0x2a1a00 = "/" + _0x4c05a2[_0x29beb7][0] + "/500/" + _0x2ac0ce + ".m3u8",
-      _0x2ba36e = ku9.md5(_0x300dce + _0x2a1a00 + _0x1aa141),
-      _0x330469 = _0x4c05a2[_0x29beb7][1] + (_0x2a1a00 + "?sign=" + _0x2ba36e + "&t=" + _0x1aa141);
-    return JSON.stringify({
-      "url": _0x330469,
-      "headers": _0xbabeac
-    });
-  }
 }
